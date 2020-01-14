ee846807470d9935b4b308e9151c05df69fb1f95
luizosb <56880799+luizosb@users.noreply.github.com>
2020-01-13 21:47:25 -0300

    Programa Teste
    Leitor de pessoas: Nome e altura

---
+++ ProgramaTeste2/Pessoa.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProgramaTeste2
+{
+    class Pessoa
+    {
+        public string nome;
+        public double altura;
+
+        public void faleSobreVoce()
+        {
+            Console.WriteLine("Meu nome é " + this.nome + ".");
+            Console.WriteLine("Minha altura é de " + this.altura + " metros.");
+        }
+    }
+}
---
+++ ProgramaTeste2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace ProgramaTeste2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String entrada = "";
+
+            Console.WriteLine("------------------------------------------------------------------------------");
+
+            Pessoa pessoa1;
+            pessoa1 = new Pessoa();
+            
+            Console.WriteLine("Este programa lê o seu nome e sua altura! Três pessoas serão analisadas!");
+            Console.WriteLine("Agora vamos ler da 1º pessoa.");
+            Console.WriteLine("Digite seu nome: ");
+            entrada = Console.ReadLine();
+            pessoa1.nome = entrada;
+            Console.WriteLine("Digite sua altura: ");
+            entrada = Console.ReadLine();
+            pessoa1.altura = Convert.ToDouble(entrada);
+            pessoa1.faleSobreVoce();
+
+            Console.WriteLine("------------------------------------------------------------------------------");
+            
+
+            Pessoa pessoa2;
+            pessoa2 = new Pessoa();
+
+            Console.WriteLine("Agora vamos ler da 2º pessoa.");
+            Console.WriteLine("Digite seu nome: ");
+            entrada = Console.ReadLine();
+            pessoa2.nome = entrada;
+            Console.WriteLine("Digite sua altura: ");
+            entrada = Console.ReadLine();
+            pessoa2.altura = Convert.ToDouble(entrada);
+            pessoa2.faleSobreVoce();
+
+            Console.WriteLine("------------------------------------------------------------------------------");
+
+            Pessoa pessoa3;
+            pessoa3 = new Pessoa();
+
+            Console.WriteLine("Agora vamos ler da 3º pessoa.");
+            Console.WriteLine("Digite seu nome: ");
+            entrada = Console.ReadLine();
+            pessoa3.nome = entrada;
+            Console.WriteLine("Digite sua altura: ");
+            entrada = Console.ReadLine();
+            pessoa3.altura = Convert.ToDouble(entrada);
+            pessoa3.faleSobreVoce();
+            Console.WriteLine("------------------------------------------------------------------------------");
+            Console.ReadKey();
+                                        
+        }
+    }
+}
